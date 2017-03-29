4d74f1e8900a7833a8c4480e0615008a8eaff8de
unknown <obr@WKSOBR02.lanexpert.grp>
2017-03-29 09:50:54 +0200

    Debug Setup perfmon for named instance

--- SQLWaitStats/src/Setup_PerfMon_counters.ps1
+++ SQLWaitStats/src/Setup_PerfMon_counters.ps1
@@ -11,12 +11,13 @@
 #  - 0.2 - 06/12/2016 : Ask for MS SQL Instance destination
 #  - 0.3 - 19/12/2016 : Changes on DSN creation
 #  - 0.4 - 05/02/2017 : Add display on collected metrics + Change collect frequency to 15s + Debug for standard instance
+#  - 1.0 - 29/03/2017 : Add metric "Avg. Disk Bytes/Transfert"
 
 ## Import functions
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){
-    Write-Output "Must execute script with Administrator rights"
-    exit -1
+	Write-Output "Must execute script with Administrator rights"
+	exit -1
 }
 
 #Perfmon DataCollector Name
@@ -29,6 +30,7 @@ $OS_Counters = "
 \PhysicalDisk(*)\% Disk Time
 \PhysicalDisk(*)\Avg. Disk Queue Length
 \PhysicalDisk(*)\Avg. Disk sec/Transfert
+\PhysicalDisk(*)\Avg. Disk Bytes/Transfert
 \PhysicalDisk(*)\Disk Read Bytes/sec
 \PhysicalDisk(*)\Disk Write Bytes/sec
 
@@ -51,12 +53,12 @@ Set-Content $pwd\$cntrname.txt $OS_Counters
 ## Loop on all SQL instances
 foreach ($AnInstanceName in $Instances)
 {
-            if ($($AnInstanceName).ToString() -eq "MSSQLSERVER"){
-                        $AnInstanceName="SQLServer"
-            }else{
-                        $AnInstanceName="MSSQL`$"+$AnInstanceName
-            }
-    $SQL_Counters = "
+	if ($($AnInstanceName).ToString() -eq "MSSQLSERVER"){
+		$AnInstanceName="SQLServer"
+	}else{
+		$AnInstanceName="MSSQL`$"+$AnInstanceName
+	}
+	$SQL_Counters = "
 \<INSTANCE>:Access Methods\Full Scans/sec
 \<INSTANCE>:Access Methods\Index Searches/sec
 \<INSTANCE>:Access Methods\Page Splits/sec
@@ -80,7 +82,7 @@ foreach ($AnInstanceName in $Instances)
 \<INSTANCE>:SQL Statistics\Batch Requests/sec
 \<INSTANCE>:SQL Statistics\SQL Compilations/sec
 \<INSTANCE>:SQL Statistics\SQL Re-Compilations/sec"
-            $SQL_Counters | % {$_ -replace "<INSTANCE>", $AnInstanceName} | Add-Content $pwd\$cntrname.txt
+	$SQL_Counters | % {$_ -replace "<INSTANCE>", $AnInstanceName} | Add-Content $pwd\$cntrname.txt
 }
 
 # Create the Perfmon DataCollector
@@ -93,70 +95,73 @@ Write-Output " yes `n no`n(default:no)"
 $Ans = Read-Host
 
 if ($Ans -eq "yes"){
-                                   $MSSQL_destination = $($Instances[0])
-                                   # Select SQL Server instance destination
-                                   if ($Instances.count -gt 1){
-                                                  $Ans = -1
-                                                  $i = 0
-                                                  while ($Ans -lt 0 -or $Ans -gt $($i-1)){
-                                                  $i=0
-                                                  Write-Output "Choose the instance hosting the metrics"
-                                                  foreach ($AnInstanceName in $Instances)
-                                                  { Write-Output "$($i): $AnInstanceName" ; $i++}
-                                                  $Ans = Read-Host
-                                                  }
-                                                  $MSSQL_destination=$($Instances[$Ans])
-                                   }else{
-                                                  $MSSQL_destination=$($Instances[0])
-                                   }
-                                   Write-Output "Metric statistics destination : [$MSSQL_destination].[msdb]"
+	$MSSQL_destination = $($Instances[0])
+	# Select SQL Server instance destination
+	if ($Instances.count -gt 1){
+		$Ans = -1
+		$i = 0
+		while ($Ans -lt 0 -or $Ans -gt $($i-1)){
+			$i=0
+			Write-Output "Choose the instance hosting the metrics"
+			foreach ($AnInstanceName in $Instances)
+			{ Write-Output "$($i): $AnInstanceName" ; $i++}
+			$Ans = Read-Host
+		}
+		$MSSQL_destination=$($Instances[$Ans])
+	}else{
+		$MSSQL_destination=$($Instances[0])
+	}
+	Write-Output "Metric statistics destination : [$MSSQL_destination].[msdb]"
 
-            # Drop DataCollector if already exists
-            try{
-                        $DataCollectorSet = new-object -COM Pla.DataCollectorSet
-                        $($datacollectorset.Query($cntrname,$null))
-                        $datacollectorset.stop($false)
-                        Write-Output "Stop DataCollectorSet"
-                        $datacollectorset.delete()
-                        Write-Output "Delete DataCollectorSet"
-                        Start-Sleep -s 2
-            } catch {
-                        Write-Output "DataCollector cleanup not done !"
-            }
+	# Drop DataCollector if already exists
+	try{
+		$DataCollectorSet = new-object -COM Pla.DataCollectorSet
+		$($datacollectorset.Query($cntrname,$null))
+		$datacollectorset.stop($false)
+		Write-Output "DataCollectorSet stoped"
+		Start-Sleep -s 2
+		$datacollectorset.delete()
+		Write-Output "DataCollectorSet deleted"
+		Start-Sleep -s 2
+	} catch {
+		Write-Output "DataCollector cleanup not done !"
+	}
 
-           #Create the connection to database (store metric values)
-           if ($(get-odbcdsn | where name -eq $cntrname) ){
-                       Write-Output "DSN already exists"
-           } else {
-                if($MSSQL_destination -eq "MSSQLSERVER"){
-                      Add-OdbcDsn -Name $cntrname -DriverName "SQL Server" -DsnType "System" -SetPropertyValue @("Server=localhost", "Trusted_Connection=Yes", "Database=msdb")
-                }else{
-                      Add-OdbcDsn -Name $cntrname -DriverName "SQL Server" -DsnType "System" -SetPropertyValue @("Server=localhost\$MSSQL_destination", "Trusted_Connection=Yes", "Database=msdb")
-                }
-           }
+	#Create the connection to database (store metric values)
+	if ($(get-odbcdsn | where name -eq $cntrname) ){
+		Write-Output "DSN already exists ; trying to remove and then recreate"
+		Remove-OdbcDsn -Name $cntrname -DsnType "System"
+	}
 
-           # Create the DataCollector
-           #Write-Output $strCMD
-           Invoke-Expression $strCMD
+	if($MSSQL_destination -eq "MSSQLSERVER"){
+		Add-OdbcDsn -Name $cntrname -DriverName "SQL Server" -DsnType "System" -SetPropertyValue @("Server=localhost", "Trusted_Connection=Yes", "Database=msdb")
+	}else{
+		Add-OdbcDsn -Name $cntrname -DriverName "SQL Server" -DsnType "System" -SetPropertyValue @("Server=localhost\$MSSQL_destination", "Trusted_Connection=Yes", "Database=msdb")
+	}
 
-           #Start the DataCollector
-           try{
-                       $DataCollectorSet = new-object -COM Pla.DataCollectorSet
-                       $datacollectorset.Query($cntrname,$null)
-                       $datacollectorset.start($false)
-                                                              Write-Output "DataCollector started..."
-           }catch {
-                       Write-Output "DataCollector not started..."
-           }
-                           Write-Output "Wait a little then check the saved metrics from SQL Server..."
-                           Start-Sleep -s 30
 
-                           # Display metrics collected
-                           if($MSSQL_destination -eq "MSSQLSERVER"){
-                Invoke-Sqlcmd -Query "SELECT DISTINCT [ObjectName]+' '+ISNULL([CounterName],'')+' '+ISNULL([InstanceName],'') as CounterName FROM [msdb].[dbo].[CounterDetails] order by 1;" -ServerInstance "localhost"
-                           }else{
-                                               Invoke-Sqlcmd -Query "SELECT DISTINCT [ObjectName]+' '+ISNULL([CounterName],'')+' '+ISNULL([InstanceName],'') as CounterName FROM [msdb].[dbo].[CounterDetails] order by 1;" -ServerInstance "localhost\$MSSQL_destination"
-                           }
+	# Create the DataCollector
+	#Write-Output $strCMD
+	Invoke-Expression $strCMD
+
+	#Start the DataCollector
+	try{
+		$DataCollectorSet = new-object -COM Pla.DataCollectorSet
+		$datacollectorset.Query($cntrname,$null)
+		$datacollectorset.start($false)
+		Write-Output "DataCollector started..."
+	}catch {
+		Write-Output "DataCollector not started..."
+	}
+	Write-Output "Wait a little then check the saved metrics from SQL Server..."
+	Start-Sleep -s 30
+
+	# Display metrics collected
+	if($MSSQL_destination -eq "MSSQLSERVER"){
+		Invoke-Sqlcmd -Query "SELECT DISTINCT [ObjectName]+' '+ISNULL([CounterName],'')+' '+ISNULL([InstanceName],'') as CounterName FROM [msdb].[dbo].[CounterDetails] order by 1;" -ServerInstance "localhost"
+	}else{
+		Invoke-Sqlcmd -Query "SELECT DISTINCT [ObjectName]+' '+ISNULL([CounterName],'')+' '+ISNULL([InstanceName],'') as CounterName FROM [msdb].[dbo].[CounterDetails] order by 1;" -ServerInstance "localhost\$MSSQL_destination"
+	}
 }
 
 exit 0
